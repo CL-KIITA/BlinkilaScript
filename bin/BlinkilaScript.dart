b633c69979ab83529b9281b250736c95e4c502db
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-09-09 03:11:12 +0900

    add: output

--- bin/BlinkilaScript.dart
+++ bin/BlinkilaScript.dart
@@ -10,8 +10,16 @@ void main(List<String> args) {
     return request.close();
   }).then((HttpClientResponse response) {
     response.transform(utf8.decoder).listen((contents) {
-      List<List<String>> rowsAsListOfValues = contents.split("\n").map((String row)=>row.split(", ")).toList();
-      print(rowsAsListOfValues);
+      List<List<String>> rowsAsListOfValues = contents.split("\n").map((String row)=>row.split(",")).toList();
+      List<List<String>> rowsAsListOfValuesFd = rowsAsListOfValues.skip(1).where((List<String> row)=>row.length==3).map((List<String> row){
+        if(row[0].contains("\"\"\"\"")){
+          row[0]="\"";
+        }
+        return row;
+      }).toList();
+      List<String> asciiList = rowsAsListOfValuesFd.map((List<String> row)=>row[0]).toList();
+      List<List<String>> morseList = rowsAsListOfValuesFd.map((List<String> row)=>row.take(2).toList()).toList();
+      print(asciiList.toString()+"\n\n"+morseList.toString()+"\n\n");
     });
   });
 }
